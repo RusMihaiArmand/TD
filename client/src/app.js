2307c523ceea48f63465f29415a35489e7b49201
Mihai-Armand <mihai_arm@yahoo.com>
2023-05-17 22:43:01 +0300

    better functions for all tables

--- client/src/app.js
+++ client/src/app.js
@@ -5,21 +5,26 @@ var data = {}
 
 function run() {
   new Vue({
-    el: '#app',
+    //el: '#app',
     data: {
         users: [],
-        testing: []
+        games: [],
+        borrows:[]
     },
     created: function () {
         this.getUsers().then(response => (this.users = response.data));
-        this.getTest().then(response => (this.testing = response.data));
+        this.getGames().then(response => (this.games = response.data));
+        this.getBorrows().then(response => (this.borrows = response.data));
     },
     methods: {
       getUsers: function() {
           return axios.get('http://localhost:3000/users');
         },
-        getTest: function () {
-            return axios.get('http://localhost:3000/tester');
+        getGames: function () {
+            return axios.get('http://localhost:3000/games');
+        },
+        getBorrows: function () {
+            return axios.get('http://localhost:3000/borrowings');
         }
     }
   });
@@ -30,126 +35,378 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
-function testFunction() {
-    //console.log(5 + 4);
-
-    //data.table = []
-    //for (i = 0; i < 6; i++) {
-    //    var obj = {
-    //        id: i,
-    //        square: i * i
-    //    }
-    //    data.table.push(obj)
-    //}
 
 
-    for (i = 1; i < 2; i++) {
-        var obj = {
-            id: i,
-            square: i * i
+//document.querySelector("#book-list").addEventListener("click", (e) => {
+
+//    target = e.target;
+//    if (target.classList.contains("delete")) {
+
+//        target.parentElement.parentElement.remove();
+
+//    }
+
+//});
+
+
+//document.querySelector("#book-form").addEventListener("submit", (e) => {
+
+//    e.preventDefault();
+
+//    const title = document.querySelector("#titleInput").value;
+//    const author = document.querySelector("#authorInput").value;
+//    const bookType = document.querySelector("#typeInput").value;
+
+//    if (title == "" || author == "" ||  bookType == "")
+//    {
+//        //to add
+//    }
+//    else
+//    {
+//        if (currentRow == null) {
+
+//            const list = document.querySelector("#book-list");
+//            const row = document.createElement("tr");
+
+//            row.innerHTML = `
+//                <td> ${title} </td >
+//                <td> ${author} </td>
+//                <td> ${bookType} </td>
+
+//                <td>
+//                     <a class="modify" href="#"> Modify </a>
+//                     <a class="delete" href="#"> Delete </a>
+//                </td>
+//            `;
+
+//            list.appendChild(row);
+//            currentRow = null;
+//        }
+//        else {
+//            currentRow.children[0].textContent = title;
+//            currentRow.children[1].textContent = author;
+//            currentRow.children[2].textContent = bookType;
+
+//            currentRow = null;
+
+
+//        }
+//    }
+
+
+//});
+
+//document.querySelector("#book-list").addEventListener("click", (e) => {
+
+//    target = e.target;
+
+//    if (target.classList.contains("modify")) {
+
+//        currentRow = target.parentElement.parentElement;
+//        document.querySelector("#titleInput").value = currentRow.children[0].textContent;
+//        document.querySelector("#authorInput").value = currentRow.children[1].textContent;
+//        document.querySelector("#typeInput").value = currentRow.children[2].textContent;
+//    }
+
+//});
+
+
+
+
+function GetGame(){
+    console.log("R");
+
+    let idText = $('#gameId').val();
+    let id = parseInt(idText);
+    console.log(id);
+
+
+    var game;
+    document.querySelector("#gameName").value = "";
+    document.querySelector("#gameGenre").value = "";
+
+    $("#comd").html("wait");
+
+    axios.get('http://localhost:3000/games/' + id).then(
+        (response) => {
+  
+            if (response.data === 'not found') {
+                console.log('cool');
+                document.querySelector("#gameName").value = "NA";
+                document.querySelector("#gameGenre").value = "NA";
+                document.querySelector("#gameStatus").value = "NA";
+            }
+            else {
+                console.log(response.data);
+                console.log('huh');
+                document.querySelector("#gameName").value = response.data.name;
+                document.querySelector("#gameGenre").value = response.data.genre;
+                document.querySelector("#gameStatus").value = response.data.status;
+            }
+            
+            $("#comd").html("ready");
         }
+    );
+}
+
+function AddGame() {
+    console.log("C");
+
+    $("#comd").html("wait");
+    var obj = {
+        id: 69,
+        name: document.querySelector("#gameName").value,
+        genre: document.querySelector("#gameGenre").value,
+        status: "available"
     }
-    console.log(obj);
-    console.log(JSON.stringify(obj));
 
-
-    data.table = []
-    for (i = 0; i < 6; i++) {
-        var obj = {
-            id: i,
-            square: i * i
+    axios.put('http://localhost:3000/games', obj).then(
+        (response) => {
+            $("#comd").html("ready");
         }
-        data.table.push(obj)
+    );
+
+
+}
+
+function UpdateGame() {
+    console.log("U");
+
+    $("#comd").html("wait");
+    var obj = {
+        id: document.querySelector("#gameId").value,
+        name: document.querySelector("#gameName").value,
+        genre: document.querySelector("#gameGenre").value,
+        status: document.querySelector("#gameStatus").value
     }
 
-    axios.put('http://localhost:3000/tester', obj);
+    axios.post('http://localhost:3000/games', obj).then(
+        (response) => {
+            $("#comd").html("ready");
+        }
+    );
 
 
-    
+}
 
-    //console.log(data);
-    //console.log(JSON.stringify(data));
+function DeleteGame() {
+    console.log("D");
 
-    // axios.put('http://localhost:3000/tester', data)  ;
+    let idText = $('#gameId').val();
+    let id = parseInt(idText);
+    console.log(id);
 
-    //fs.writeFile("input.json", JSON.stringify(data), function (err) {
-    //    if (err) throw err;
-    //    console.log('complete');
-    //}
-    //);
+    $("#comd").html("wait");
+
+    axios.delete('http://localhost:3000/games/' + id).then(
+        (response) => {
+            $("#comd").html("ready");
+        }
+    );
 
 }
 
 
-document.querySelector("#book-list").addEventListener("click", (e) => {
-
-    target = e.target;
-    if (target.classList.contains("delete")) {
-
-        target.parentElement.parentElement.remove();
-
-    }
-
-});
 
 
-document.querySelector("#book-form").addEventListener("submit", (e) => {
 
-    e.preventDefault();
+function GetUser() {
 
-    const title = document.querySelector("#titleInput").value;
-    const author = document.querySelector("#authorInput").value;
-    const bookType = document.querySelector("#typeInput").value;
+    let userN = $('#userName').val();
 
-    if (title == "" || author == "" ||  bookType == "")
-    {
-        //to add
-    }
-    else
-    {
-        if (currentRow == null) {
 
-            const list = document.querySelector("#book-list");
-            const row = document.createElement("tr");
+    var user;
 
-            row.innerHTML = `
-                <td> ${title} </td >
-                <td> ${author} </td>
-                <td> ${bookType} </td>
+    document.querySelector("#userPass").value = "";
 
-                <td>
-                     <a class="modify" href="#"> Modify </a>
-                     <a class="delete" href="#"> Delete </a>
-                </td>
-            `;
+    $("#comd").html("wait");
 
-            list.appendChild(row);
-            currentRow = null;
+    axios.get('http://localhost:3000/users/' + userN).then(
+        (response) => {
+
+            if (response.data === 'not found') {
+
+                document.querySelector("#userPass").value = "NA";
+
+            }
+            else {
+                console.log(response.data);
+
+                document.querySelector("#userPass").value = response.data.password;
+
+            }
+
+            $("#comd").html("ready");
         }
-        else {
-            currentRow.children[0].textContent = title;
-            currentRow.children[1].textContent = author;
-            currentRow.children[2].textContent = bookType;
+    );
+}
 
-            currentRow = null;
+function AddUser() {
 
 
+    $("#comd").html("wait");
+    var obj = {
+        name: document.querySelector("#userName").value,
+        password: document.querySelector("#userPass").value,
+    }
+
+    axios.put('http://localhost:3000/users', obj).then(
+        (response) => {
+            $("#comd").html("ready");
         }
+    );
+
+
+}
+
+function UpdateUser() {
+
+
+    $("#comd").html("wait");
+    var obj = {
+        name: document.querySelector("#userName").value,
+        password: document.querySelector("#userPass").value,
+        newpassword: document.querySelector("#newuserPass").value
     }
 
+    axios.post('http://localhost:3000/users', obj).then(
+        (response) => {
+            $("#comd").html("ready");
+        }
+    );
 
-});
 
-document.querySelector("#book-list").addEventListener("click", (e) => {
+}
 
-    target = e.target;
+function DeleteUser(){
 
-    if (target.classList.contains("modify")) {
+    let userN = $('#userName').val();
 
-        currentRow = target.parentElement.parentElement;
-        document.querySelector("#titleInput").value = currentRow.children[0].textContent;
-        document.querySelector("#authorInput").value = currentRow.children[1].textContent;
-        document.querySelector("#typeInput").value = currentRow.children[2].textContent;
+
+    $("#comd").html("wait");
+
+    axios.delete('http://localhost:3000/users/' + userN).then(
+        (response) => {
+            $("#comd").html("ready");
+        }
+    );
+
+}
+
+
+function getBorrows2() {
+    return axios.get('http://localhost:3000/borrowings');
+}
+
+
+
+function HowMany() {
+    let userN = $('#userName').val();
+    let cont = 0;
+    console.log("goins");
+    $("#comd").html("wait");
+
+
+    var number = 0;
+
+    axios.get('http://localhost:3000/borrowings' ).then(
+        (response) => {
+
+            for (let i = 0; i < response.data.length; i++)
+            {
+                if (response.data[i].user == userN) {
+                    cont = cont + 1;
+
+                }
+            }
+
+            //document.querySelector("#bnr").value = cont;
+            number = cont;
+            $("#comd").html("ready2");
+            document.querySelector("#bnr").value = number + " games";
+        }
+
+    );
+   
+   
+    //axios.get('http://localhost:3000/borrowings/' + userN).then(
+    //    (response) => {
+
+    //        console.log("A");
+    //        console.log(response.data);
+
+    //        document.querySelector("#bnr").value = response.data;
+
+    //        $("#comd").html("ready");
+    //    }
+    //);
+
+    
+}
+
+
+
+function BorrowGame() {
+
+
+    $("#comd").html("wait");
+    var obj = {
+        gameid: document.querySelector("#borrowgameId").value,
+        user: document.querySelector("#userName").value
     }
 
-});
+    axios.put('http://localhost:3000/borrowings', obj).then(
+        (response) => {
+            $("#comd").html("ready");
+        }
+    );
+
+
+}
+
+function ReturnGame() {
+
+
+    $("#comd").html("wait");
+
+
+    var gameid = document.querySelector("#borrowgameId").value;
+    
+
+    axios.delete('http://localhost:3000/borrowings/' + gameid).then(
+        (response) => {
+            $("#comd").html("ready");
+        }
+    );
+
+
+}
+
+
+function WhoHasIt() {
+
+    let id = $('#borrowgameId').val();
+
+
+    document.querySelector("#userName").value = "";
+
+    $("#comd").html("wait");
+
+    axios.get('http://localhost:3000/borrowings/' + id).then(
+        (response) => {
+
+            if (response.data === 'not found') {
+
+                document.querySelector("#userName").value = "NOBODY";
+
+            }
+            else {
+
+                document.querySelector("#userName").value = response.data.user;
+
+            }
+
+            $("#comd").html("ready");
+        }
+    );
+}
--- server/index.js
+++ server/index.js
@@ -1,81 +1,40 @@
 var api = require('./src/api.js').app;
 var users = require('./src/users.json');
-var tester = require('./src/tester.json');
+//var games = require('./src/games.json');
+const gamesPath = './src/games.json';
+const usersPath = './src/users.json';
+const borrowPath = './src/borrowings.json';
 var fs = require('fs');
-var data2;
+
+
+
 
 api.get('/', function (request, response) {
   response.json('NodeJS REST API');
 });
 
-api.get('/users', function (request, response) {
-  response.json(users);
-});
+//api.get('/users', function (request, response) {
+//  response.json(users);
+//});
 
-api.get('/tester', function (request, response) {
-    response.json(tester);
-});
+//api.get('/tester', function (request, response) {
+//    response.json(tester);
+//});
 
-api.put('/users', function (request, response) {
-  users[users.length] = request.body;
-  response.json('User was saved succesfully');
+//api.put('/users', function (request, response) {
+//  users[users.length] = request.body;
+//  response.json('User was saved succesfully');
 
-});
-
-api.put('/tester', function (request, response) {
-    tester[tester.length] = request.body;
-    response.json('tester was saved succesfully');
-
-    //fs.writeFileSync(tester, JSON.stringify(request));
-
-    //fs.writeFileSync('./src/tester.json', JSON.stringify(request.body), function (err) {
-    //    if (err) throw err;
-    //    console.log('complete2');
-    //}
-    //);
-
-
-    fs.writeFileSync('./src/tester.json', JSON.stringify(tester), function (err) {
-        if (err) throw err;
-        console.log('complete1');
-    }
-    );
+//});
 
 
 
-    console.log('complete2');
-    //thing2(data);
+//api.delete('/users/:index', function (request, response) {
+//  users.splice(request.params.index, 1);
+//  response.json('User with index ' + request.params.index + ' was deleted');
+//});
 
 
-
-    //fs.writeFile('./src/tester.json', JSON.stringify(data), function (err) {
-    //    if (err) throw err;
-    //    console.log('complete');
-    //}
-    //);
-
-
-    //tester[tester.length] = request.body;
-    //response.json('tester was saved succesfully');
-
-
-
-});
-
-function thing2(dataa) {
-    
-    fs.writeFileSync(tester, JSON.stringify(dataa));
-
-}
-
-
-
-
-
-api.delete('/users/:index', function (request, response) {
-  users.splice(request.params.index, 1);
-  response.json('User with index ' + request.params.index + ' was deleted');
-});
 
 api.listen(3000, function () {
   console.log('Server running @ localhost:3000');
@@ -86,30 +45,403 @@ api.listen(3000, function () {
 
 
 
-//const sql = require('mssql/msnodesqlv8');
 
-//var config={
-//    database: 'TDbase',
-//    server: 'DESKTOP-A5P03KC',
-//    driver: 'msnodesqlv8',
-//    options: {
-//        trustedConnection:true
-//    }
-//};
+//-------- GAMES
 
-//sql.connect(config, function (err) {
+api.get('/games', function (request, response) {
+    response.json(getGames());
+});
+
+api.get('/games/:id', function (request, response) {
+    let game = getGameById(request.params.id);
+    if (game) response.json(game);
+    else response.json('not found');
+});
+
+function getGames() {
+    let gamesList = [];
+    try {
+        gamesList = JSON.parse(fs.readFileSync(gamesPath, 'utf8'));
+    } catch (err) {
+        console.error(err);
+        return false;
+    }
+    return gamesList;
+}
+
+function getGamesMaxId(gamesList) {
+    let max = 0;
+    for (var i = 0; i < gamesList.length; i++) {
+        if (max < parseInt(gamesList[i].id)) {
+            max = parseInt(gamesList[i].id);
+        }
+    }
+    return max;
+}
+
+function getGameById(id) {
+    let gamesList = getGames();
+    let selectedGame = null;
+    for (var i = 0; i < gamesList.length; i++) {
+        if (id == gamesList[i].id) selectedGame = gamesList[i];
+    }
+    return selectedGame;
+}
+
+api.put('/games', function (request, response) {
+    saveGame(request.body);
+    response.json('finished');
+});
+
+function saveGame(game) {
+    let gamesList = getGames();
+    let maxIdTxt = getGamesMaxId(gamesList); 
+    let maxId = parseInt(maxIdTxt);
+    game.id = (maxId + 1).toString();
+    gamesList.push(game);
+    try {
+        fs.writeFileSync(gamesPath, JSON.stringify(gamesList));
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+api.post('/games', function (request, response) {
+
+    let game = request.body;
+    let gamesList = getGames();
+    for (let i = 0; i < gamesList.length; i++) {
+        if (gamesList[i].id === game.id) {
+            gamesList[i] = game;
+        }
+    }
 
 
-//    if (err) console.log(err);
-//    //var request = new sql.Request();
+    try {
+        fs.writeFileSync(gamesPath, JSON.stringify(gamesList));
+    } catch (err) {
+        console.error(err)
+    }
+    finally {
+        response.json('finished');
+    }
+});
 
-//    //request.query('select * from thing', function (err, recordSet) {
+api.delete('/games/:index', function (request, response) {
 
-//    //    if (err) console.log(err);
-//    //     else  console.log(recordSet);
-        
+    let id = request.params.index;
+    let indDel = -1;
+    let gamesList = getGames();
 
-//    //})
+    for (let i = 0; i < gamesList.length; i++) {
+        if (gamesList[i].id === id) {
+            indDel = i;
+        }
+    }
 
-//})
+    if (indDel >= 0) {
+
+        gamesList.splice(indDel, 1);
+    }
+
+
+    try {
+        fs.writeFileSync(gamesPath, JSON.stringify(gamesList));
+    } catch (err) {
+        console.error(err)
+    }
+    finally {
+        response.json('finished');
+    }
+
+});
+
+
+
+
+
+
+
+//--------BORROW
+
+api.get('/borrowings', function (request, response) {
+    response.json(getBorr());
+});
+
+api.get('/borrowings/:id', function (request, response) {
+    let borr = getBorrById(request.params.id);
+    if (borr) response.json(borr);
+    else response.json('not found');
+});
+
+function getBorrById(id) {
+    let borrList = getBorr();
+    let selectedBorr = null;
+    for (var i = 0; i < borrList.length; i++) {
+        if (id == borrList[i].gameid) selectedBorr = borrList[i];
+    }
+    return selectedBorr;
+}
+
+
+function getBorr() {
+    let borrList = [];
+    try {
+        borrList = JSON.parse(fs.readFileSync(borrowPath, 'utf8'));
+    } catch (err) {
+        console.error(err);
+        return false;
+    }
+    return borrList;
+}
+
+api.get('/borrowings/:id', function (request, response) {
+    let userN = request.params.id;
+    var ctr = 0;
+    let borrList = getBorr();
+
+        for (var i = 0; i < borrList.length; i++) {
+            if (userN == borrList[i].user)
+            {
+                ctr = ctr + 1;
+            }
+        }
+   
+        return ctr;
+    
+});
+
+
+
+api.put('/borrowings', function (request, response) {
+    saveBorr(request.body);
+    response.json('finished');
+});
+
+function saveBorr(borr) {
+    let borrList = getBorr();
+    let gamesList = getGames();
+    let userList = getUsers();
+
+    let ok = false;
+    var gameIndex = -1;
+
+    for (var i = 0; i < gamesList.length; i++) {
+        if (borr.gameid == gamesList[i].id) {
+            ok = true;
+            gameIndex = i;
+        }
+    }
+
+    let ok2 = false;
+    for (var i = 0; i < userList.length; i++) {
+        if (borr.user == userList[i].name) {
+            ok2 = true;
+        }
+    }
+
+
+    for (var i = 0; i < borrList.length; i++) {
+        if ( borr.gameid == borrList[i].gameid)
+        {
+            ok = false;
+        }
+    }
+
+
+    
+
+
+    let count = 0;
+    for (let i = 0; i < borrList.length; i++) {
+        if (borr.user == borrList[i].user) {
+            count = count + 1;
+
+        }
+    }
+
+    if (count >= 3)
+    {
+        ok = false;
+    }
+
+
+    if (ok == true && ok2 == true) {
+
+        borrList.push(borr);
+        try {
+            fs.writeFileSync(borrowPath, JSON.stringify(borrList));
+
+
+            gamesList[gameIndex].status = "unavailable";
+            fs.writeFileSync(gamesPath, JSON.stringify(gamesList));
+
+
+        } catch (err) {
+            console.error(err);
+        }
+    }
+}
+
+
+api.delete('/borrowings/:index', function (request, response) {
+
+    let id = request.params.index;
+    let indDel = -1;
+    let borrList = getBorr();
+
+    for (let i = 0; i < borrList.length; i++) {
+        if (borrList[i].gameid === id) {
+            indDel = i;
+        }
+    }
+
+    if (indDel >= 0) {
+
+        borrList.splice(indDel, 1);
+    }
+
+
+    let gamesList = getGames();
+
+    var gameIndex = -1;
+
+    for (var i = 0; i < gamesList.length; i++) {
+        if (id == gamesList[i].id) {
+            gameIndex = i;
+        }
+    }
+
+
+
+    try {
+        fs.writeFileSync(borrowPath, JSON.stringify(borrList));
+        gamesList[gameIndex].status = "available";
+        fs.writeFileSync(gamesPath, JSON.stringify(gamesList));
+    } catch (err) {
+        console.error(err)
+    }
+    finally {
+        response.json('finished');
+    }
+    
+
+});
+
+
+
+
+
+//-------- USER
+
+api.get('/users', function (request, response) {
+    response.json(getUsers());
+});
+
+api.get('/users/:id', function (request, response) {
+    let user = getUserByName(request.params.id);
+    if (user) response.json(user);
+    else response.json('not found');
+});
+
+function getUsers() {
+    let userList = [];
+    try {
+        userList = JSON.parse(fs.readFileSync(usersPath, 'utf8'));
+    } catch (err) {
+        console.error(err);
+        return false;
+    }
+    return userList;
+}
+
+
+function getUserByName(nam) {
+    let userList = getUsers();
+    let selectedUser = null;
+    for (var i = 0; i < userList.length; i++) {
+        if (nam == userList[i].name) selectedUser = userList[i];
+    }
+    return selectedUser;
+}
+
+api.put('/users', function (request, response) {
+    saveUser(request.body);
+    response.json('finished');
+});
+
+function saveUser(user) {
+    let userList = getUsers();
+    let ok = true;
+    for (var i = 0; i < userList.length; i++) {
+        if (user.name == userList[i].name) ok = false;
+    }
+
+    if (ok == true) {
+        userList.push(user);
+        try {
+            fs.writeFileSync(usersPath, JSON.stringify(userList));
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+}
+
+api.post('/users', function (request, response) {
+
+    let user = request.body;
+
+    var obj = {
+        name: user.name,
+        password: user.newpassword
+    }
+
+    let userList = getUsers();
+    for (let i = 0; i < userList.length; i++) {
+        if (userList[i].name === user.name && userList[i].password === user.password) {
+            userList[i] = obj;
+        }
+    }
+
+
+    try {
+        fs.writeFileSync(usersPath, JSON.stringify(userList));
+    } catch (err) {
+        console.error(err)
+    }
+    finally {
+        response.json('finished');
+    }
+});
+
+api.delete('/users/:index', function (request, response) {
+
+    let num = request.params.index;
+    let indDel = -1;
+    let userList = getUsers();
+
+    for (let i = 0; i < userList.length; i++) {
+        if (userList[i].name === num) {
+            indDel = i;
+        }
+    }
+
+    if (indDel >= 0) {
+        userList.splice(indDel, 1);
+    }
+
+
+    try {
+        fs.writeFileSync(usersPath, JSON.stringify(userList));
+    } catch (err) {
+        console.error(err)
+    }
+    finally {
+        response.json('finished');
+    }
+
+});
+
+
 
